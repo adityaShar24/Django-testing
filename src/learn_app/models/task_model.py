46e30fecb76461310de6b3b7ca70236c1d2a3fe9
adityaShar24 <adityarajansharma1234@gmail.com>
2024-01-10 16:00:20 +0530

    Added task model

---
+++ src/learn_app/models/task_model.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    
+    title = models.CharField(max_length=255)
+    
+    is_completed = models.BooleanField(default=False)
+    
+    date = models.DateTimeField(auto_now_add=True)
+    
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
