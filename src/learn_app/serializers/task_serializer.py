0811450f9aa0c5e6db1cfcd85d66a88c5ec045a1
adityaShar24 <adityarajansharma1234@gmail.com>
2024-01-12 11:22:44 +0530

    refactor

--- src/learn_app/serializers/task_serializer.py
+++ src/learn_app/serializers/task_serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from ..models.task_model import Task
-
+from django.core.validators import RegexValidator
 
 class TaskSerializer(serializers.ModelSerializer):
     
+    title = serializers.CharField(validators=[RegexValidator(regex='^[a-zA-Z]*$', message='Only letters are allowed.', code='invalid_title')])
     class Meta:
         
         model = Task
