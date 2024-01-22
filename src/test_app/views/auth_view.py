43cd95df460f0c57cc3e64e756a4521ed599e46c
adityaShar24 <adityarajansharma1234@gmail.com>
2024-01-22 12:47:17 +0530

    refactor

--- src/test_app/views/auth_view.py
+++ src/test_app/views/auth_view.py
@@ -14,66 +14,65 @@ from ..serializers.login_serializer import LoginSerializer
 class RegisterView(APIView):
     
     def post(self , request):
-        
-        response = None
-        
-        data = request.data
-        
-        serializer = RegisterSerializer(data = data)
-        
-        if serializer.is_valid():
+        try:
+            response = None
+            
+            data = request.data
+            
+            serializer = RegisterSerializer(data = data)
+            
+            serializer.is_valid(raise_exception=True)
             user = serializer.save()
             user.set_password(serializer.validated_data['password'])
             user.save()
-        
+            
             response_data = {
                 "message": USER_REGISTERED_MESSAGE,
                 "data": serializer.data
             }
-            
+                
             response = Response(response_data , HTTP_201_CREATED)
-        else:
-            response = Response(serializer.errors , status= HTTP_400_BAD_REQUEST) 
-    
+        
+        except Exception as e:
+            response = Response({"error": str(e)} , HTTP_400_BAD_REQUEST)  
+        
         return response
 
 
 class LoginView(APIView):
     
     def post(self , request):
-        
-        response = None
-        
-        data = request.data
-        
-        serializer = LoginSerializer(data = data)
+        try:
+            response = None
+            
+            data = request.data
+            
+            serializer = LoginSerializer(data = data)
 
-        if serializer.is_valid():
+            serializer.is_valid()
             username = serializer.data['username']
             password = serializer.data['password']
-            
+                
             user = authenticate(request = request , username = username , password = password)
-            
-            
+                
+                
             if user is None:
                 response = Response({"message": INVAID_CREDENTIALS_MESSAGE} , HTTP_400_BAD_REQUEST)
-            
-            access = AccessToken.for_user(user)
-            
-            
-            refresh = RefreshToken.for_user(user)
-            
-            response_data = {
-                    "message": USER_LOGGEDIN_MESSAGE.format(username = username), 
-                    "refresh": str(refresh),
-                    "access": str(access)
-                }
-            
-            
-            response = Response( response_data , status= HTTP_201_CREATED)
-            
-        else:
-            response = Response(serializer.errors , HTTP_400_BAD_REQUEST)
+                
+            else:
+                access = AccessToken.for_user(user)
+                refresh = RefreshToken.for_user(user)
+                
+                response_data = {
+                        "message": USER_LOGGEDIN_MESSAGE.format(username = username), 
+                        "refresh": str(refresh),
+                        "access": str(access)
+                    }
+                
+                response = Response( response_data , status= HTTP_201_CREATED)
+                
+        except Exception as e:
+            response = Response({"error": str(e)} , HTTP_400_BAD_REQUEST)
             
         return response
 
@@ -86,18 +85,21 @@ class ListUsersView(APIView):
 
     
     def get(self , request):
+        try:
+            response = None
+            
+            users = User.objects.all()
+            
+            serializer = RegisterSerializer(users , many = True)
+            
+            response_data = {
+                "message": ALL_USERS_FETCHED_MESSAGE,
+                "data": serializer.data
+            }
+            
+            response = Response(response_data , status= HTTP_200_OK)
         
-        response = None
-        
-        users = User.objects.all()
-        
-        serializer = RegisterSerializer(users , many = True)
-        
-        response_data = {
-            "message": ALL_USERS_FETCHED_MESSAGE,
-            "data": serializer.data
-        }
-        
-        response = Response(response_data , status= HTTP_200_OK)
-        
+        except Exception as e:
+            response = Response({"error": str(e)} , HTTP_400_BAD_REQUEST)
+            
         return response
